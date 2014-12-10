fcab5f1e170c15eae9f98e3093f116d2d04a4a0b
George Balatsouras <gbalats@gmail.com>
2014-12-10 19:20:42 +0200

    Add smooth scrolling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,39 @@
 // Obfuscate email
+$(function() {
+    $("#fake_email").click( function(e) {
+        var username = "gbalats",
+        domain = "gmail.com";
 
-$("#fake_email").click( function(e) {
-    var username = "gbalats",
-          domain = "gmail.com";
-
-    window.location.href = "mailto:" + username + "@" + domain;
+        window.location.href = "mailto:" + username + "@" + domain;
+    });
 });
 
 
 // Toggle sidebar
-
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
+$(function() {
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 });
 
 
 // Open links in new tabs
+$(function() {
+    $(".icons")
+        .find("a")
+        .not("[id='fake_email']")
+        .attr("target", "_blank");
 
-$(".icons")
-    .find("a")
-    .not("[id='fake_email']")
-    .attr("target", "_blank");
+    $("a[href^='http']")
+        .not("[href*='gbalats.github.io']")
+        .attr("target", "_blank");
+});
 
-$("a[href^='http']")
-    .not("[href*='gbalats.gihtub.io]")
-    .attr("target", "_blank");
 
 // Initialize smooth scrolling
-smoothScroll.init({
-    speed: 700
+$(function() {
+    smoothScroll.init({
+        speed: 700
+    });
 });
